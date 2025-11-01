e08ca8454e5b84a0934a0d42531d1ad0e6d6d452
Vled5445 <vled5445@mail.ru>
2025-10-31 22:04:29 -0400

    hw_ex2

--- script2.sh
+++ script2.sh
@@ -1,9 +1,17 @@
-echo "1. Текущее значение PATH:"
+echo "Текущее значение PATH:"
 echo "   $PATH"
 
 new_dir="aab"
-echo "2. Добавляем директорию '$new_dir' в PATH"
-export PATH="$PATH/$new_dir"
-echo "   Новое значение PATH:"
-echo "   $PATH"
+echo "2. Добавление директории '$new_dir' в PATH"
+export PATH="$PATH:$new_dir"
+echo "Новое значение PATH:"
+echo "$PATH"
 
+echo "Объяснение:"
+echo "Изменения PATH через терминал временные - они действуют только"
+echo "в текущей сессии терминала."
+echo
+echo "export PATH=\"\$PATH:$new_dir\""
+echo
+echo "echo 'export PATH=\"\\\$PATH:$new_dir\"' >> ~/.bashrc"
+echo "source ~/.bashrc"
